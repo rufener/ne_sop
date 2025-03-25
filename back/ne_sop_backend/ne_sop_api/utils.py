6a6ff6c276bfe500f71c4f06a44377a4b1c54d84
Matthew Parkan <matthew.parkan@gmail.com>
2025-03-25 14:53:21 +0100

    Replaced hard coded URL with variable

--- back/ne_sop_backend/ne_sop_api/utils.py
+++ back/ne_sop_backend/ne_sop_api/utils.py
@@ -200,7 +200,7 @@ class Utils(object):
         # Add description and unique identifier
         description = (
             f"Objet parlementaire: {event.item.number} - {event.item.title}\n"
-            f"Lien: https://sop.ne.ch/#/items/{event.item.id}\n"
+            f"Lien: {settings.FRONT_URL}/#/items/{event.item.uuid}\n"
             f"Date: {event.date.strftime('%d.%m.%Y')} {event.time.strftime('%H:%M:%S') if event.time else ''}\n"
             f"Événement: {event.type.name}\n"
             f"Description: {event.description}"
